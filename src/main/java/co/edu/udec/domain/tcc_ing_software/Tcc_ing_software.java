5a61679f7d7f7b03ac63476e61ed7ad2a6af1b4e
Nerian <richard080405@gmail.com>
2025-11-26 20:34:55 -0500

    Primer commit.

---
+++ src/main/java/co/edu/udec/domain/tcc_ing_software/Tcc_ing_software.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.udec.domain.tcc_ing_software;
+
+/**
+ *
+ * @author LENOVO84
+ */
+public class Tcc_ing_software {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
